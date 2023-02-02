1187e91fa1515d9259bfdc11cd60641926dd8765
Erbenio Mota <erbeniomota@gmail.com>
2023-02-02 15:43:55 -0300

    Testando a classe de dominio categoria

--- src/main/java/br/com/kennobi/cursorest/resources/CategoriaResource.java
+++ src/main/java/br/com/kennobi/cursorest/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.kennobi.cursorest.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.kennobi.cursorest.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {		
-		return "Bem Vido !";
+	public List<Categoria> listar() {		
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>() ;
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
