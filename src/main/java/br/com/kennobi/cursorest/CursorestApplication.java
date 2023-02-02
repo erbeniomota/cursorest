fb09fb7ba84b285c4db4bdf1a78e69910da8ff76
Erbenio Mota <erbeniomota@gmail.com>
2023-02-02 09:46:27 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/kennobi/cursorest/CursorestApplication.java
@@ -0,0 +1,13 @@
+package br.com.kennobi.cursorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kennobi/cursorest/CursorestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kennobi.cursorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
