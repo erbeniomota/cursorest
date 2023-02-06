48532f17538da11322b4816ee69c883323b3c781
Erbenio Mota <erbeniomota@gmail.com>
2023-02-06 10:56:38 -0300

    Criando instanciação

--- src/main/java/br/com/kennobi/cursorest/CursorestApplication.java
+++ src/main/java/br/com/kennobi/cursorest/CursorestApplication.java
@@ -1,13 +1,32 @@
 package br.com.kennobi.cursorest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursorestApplication {
+import br.com.kennobi.cursorest.domain.Categoria;
+import br.com.kennobi.cursorest.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursorestApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursorestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
