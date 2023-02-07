2ad799bae442bee5f3fa39ed57cae348a93a5988
Erbenio Mota <erbeniomota@gmail.com>
2023-02-07 11:47:22 -0300

    ajustes endpoint Categorias/id

--- src/main/java/br/com/kennobi/cursorest/services/CategoriaService.java
+++ src/main/java/br/com/kennobi/cursorest/services/CategoriaService.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 
 import br.com.kennobi.cursorest.domain.Categoria;
 import br.com.kennobi.cursorest.repositories.CategoriaRepository;
+import br.com.kennobi.cursorest.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria find(Integer id) {
-		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		} 
 
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 
 }
