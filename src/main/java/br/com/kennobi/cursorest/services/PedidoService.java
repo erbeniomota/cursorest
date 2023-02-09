e536d6155dcc9f7031de9939354729aa3145a3ca
Erbenio Mota <erbeniomota@gmail.com>
2023-02-09 13:47:00 -0300

    endpoint pedidos

---
+++ src/main/java/br/com/kennobi/cursorest/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.kennobi.cursorest.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.kennobi.cursorest.domain.Pedido;
+import br.com.kennobi.cursorest.repositories.PedidoRepository;
+import br.com.kennobi.cursorest.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
