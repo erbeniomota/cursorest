33fadaef54a1ea126bc3d8aa4bb4c6d738c36d8a
Erbenio Mota <erbeniomota@gmail.com>
2023-02-15 14:52:21 -0300

    listando todas as categorias

--- src/main/java/br/com/kennobi/cursorest/resources/CategoriaResource.java
+++ src/main/java/br/com/kennobi/cursorest/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.kennobi.cursorest.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.kennobi.cursorest.domain.Categoria;
+import br.com.kennobi.cursorest.dto.CategoriaDTO;
 import br.com.kennobi.cursorest.services.CategoriaService;
 
 @RestController
@@ -46,6 +49,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> List = service.findAll();
+		List<CategoriaDTO> listDto = List.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
